75a33b5b7736503d3dfadb077738f32eae98b88e
Serhii <serhii.sheremet@gmail.com>
2025-10-02 12:45:23 +0200

    Cleaning config page

--- PADMA/Pages/ConfigurationPage.xaml.cs
+++ PADMA/Pages/ConfigurationPage.xaml.cs
@@ -5,6 +5,9 @@ namespace PADMA.Pages
         public ConfigurationPage()
         {
             InitializeComponent();
+
+            // Принудительно закрыть бургер-меню при входе на страницу
+            Shell.Current.FlyoutIsPresented = false;
         }
 
         private async void OnCloseClicked(object sender, EventArgs e)
@@ -14,49 +17,41 @@ namespace PADMA.Pages
 
         private async void OnLanguageClicked(object sender, EventArgs e)
         {
-            Shell.Current.FlyoutIsPresented = false;
             await Shell.Current.GoToAsync(nameof(LanguagePage));
         }
 
         private async void OnFirstDayOfWeekClicked(object sender, EventArgs e)
         {
-            Shell.Current.FlyoutIsPresented = false;
             await Shell.Current.GoToAsync(nameof(FirstDayOfWeekPage));
         }
 
         private async void OnTransitsClicked(object sender, EventArgs e)
         {
-            Shell.Current.FlyoutIsPresented = false;
             await Shell.Current.GoToAsync(nameof(TransitsPage));
         }
 
         private async void OnNodesClicked(object sender, EventArgs e)
         {
-            Shell.Current.FlyoutIsPresented = false;
             await Shell.Current.GoToAsync(nameof(NodesPage));
         }
 
         private async void OnHoraClicked(object sender, EventArgs e)
         {
-            Shell.Current.FlyoutIsPresented = false;
             await Shell.Current.GoToAsync(nameof(HoraPage));
         }
 
         private async void OnMuhurtasClicked(object sender, EventArgs e)
         {
-            Shell.Current.FlyoutIsPresented = false;
             await Shell.Current.GoToAsync(nameof(MuhurtasPage));
         }
 
         private async void OnMrityuClicked(object sender, EventArgs e)
         {
-            Shell.Current.FlyoutIsPresented = false;
             await Shell.Current.GoToAsync(nameof(MrityuPage));
         }
 
         private async void OnSunriseClicked(object sender, EventArgs e)
         {
-            Shell.Current.FlyoutIsPresented = false;
             await Shell.Current.GoToAsync(nameof(SunrisePage));
         }
 
