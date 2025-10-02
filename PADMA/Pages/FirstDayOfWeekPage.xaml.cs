8d4eaf0ebf1e2d1eb99c48212b259a6f75651966
Serhii <serhii.sheremet@gmail.com>
2025-10-02 13:06:58 +0200

    config closing dialog

--- PADMA/Pages/FirstDayOfWeekPage.xaml.cs
+++ PADMA/Pages/FirstDayOfWeekPage.xaml.cs
@@ -1,29 +1,56 @@
+using Microsoft.Maui.Controls;
+using System;
+
 namespace PADMA.Pages
 {
     public partial class FirstDayOfWeekPage : ContentPage
     {
-        private string _initialValue;
+        private string _originalValue;
+        private string _currentValue;
 
         public FirstDayOfWeekPage()
         {
             InitializeComponent();
-            _initialValue = Preferences.Get("FirstDayOfWeek", "Monday");
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // Загружаем значение (пока жёстко Monday)
+            _originalValue = "Monday";
+            _currentValue = _originalValue;
+
+            // Устанавливаем RadioButton
+            MondayRadioButton.IsChecked = _originalValue == "Monday";
+            SundayRadioButton.IsChecked = _originalValue == "Sunday";
+        }
+
+        private void OnRadioButtonCheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value) // только если выбрано
+            {
+                var rb = sender as RadioButton;
+                _currentValue = rb?.Value?.ToString();
+            }
         }
 
         private async void OnCloseClicked(object sender, EventArgs e)
         {
-            string currentValue = Preferences.Get("FirstDayOfWeek", "Monday");
-            if (currentValue != _initialValue)
+            if (_currentValue != _originalValue)
             {
-                bool save = await DisplayAlert("Confirm", "Save changes?", "Yes", "No");
+                bool save = await DisplayAlert("Save changes?",
+                    $"Apply '{_currentValue}' as first day of week?",
+                    "Yes", "No");
+
                 if (save)
                 {
+                    // сохраняем выбор (позже - в DataCache/базу)
                     MessagingCenter.Send(this, "SettingsChanged");
-                    _initialValue = currentValue;
                 }
             }
 
-            await Shell.Current.GoToAsync("..");
+            await Shell.Current.GoToAsync(".."); // закрыть страницу
         }
     }
 }
